4cb0149af9a1bde5722794237b4824216dcc16af
João Rego <regojoao881@gmail.com>
2026-09-09 21:54:57 -0300

    Atualização endpoint e html

--- endpoint.php
+++ endpoint.php
@@ -5,15 +5,35 @@ try {
 } catch (Exception $e) {
     echo "Erro: " . $e->getMessage();
 }
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-$modalidade = $_GET['modalidade'];
+    if (isset($_GET["op"]) && $_GET["op"] == "jogosAgendados") {
+        $sql = "SELECT
+            j.inicio as data_hora, 
+            tc.nome_time AS time_casa, 
+            tf.nome_time AS time_fora,
+            CONCAT(m.nome_modalidade, ' ', m.sexo) AS modalidade
 
-$result = mysqli_query($conn, "SELECT 
-    j.inicio, 
+            FROM jogo j
+
+            INNER JOIN time tc 
+               ON j.Time_casa_id = tc.idTime
+
+            INNER JOIN time tf 
+                ON j.Time_fora_id = tf.idTime
+
+            INNER JOIN chave c 
+                ON tc.Chave_idChaveamento = c.idChaveamento
+
+            INNER JOIN modalidade m 
+                ON c.Modalidade_idModalidade = m.idModalidade ";
+    } elseif (isset($_GET["op"]) && $_GET["op"] == "jogosAovivo") {
+        $sql = "SELECT
+    j.inicio AS data_hora, 
     tc.nome_time AS time_casa, 
     tf.nome_time AS time_fora,
-    m.nome_modalidade, 
-    m.sexo 
+    CONCAT(m.nome_modalidade, ' ', m.sexo) AS modalidade
+
 FROM jogo j
 
 INNER JOIN time tc 
@@ -28,12 +48,19 @@ INNER JOIN chave c
 INNER JOIN modalidade m 
     ON c.Modalidade_idModalidade = m.idModalidade
 
-WHERE m.idModalidade = $modalidade");
+WHERE j.idJogo = 5";
+    } else {
+        exit(json_encode(array()));
+    }
 
-$dados = [];
 
-while ($row = mysqli_fetch_assoc($result)) {
-    $dados[] = $row;
+    $result = mysqli_query($conn, $sql);
+
+    $dados = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $dados[] = $row;
+    }
+
+    echo json_encode($dados);
 }
-
-echo json_encode($dados);
